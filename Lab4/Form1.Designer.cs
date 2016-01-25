82de98bfa1be2aecf8c26ceb83224c103de3adc7
Zach Sarkis <zacherysarkis@gmail.com>
2016-01-24 23:23:10 -0500

    Eight Queens puzzle
    The goal of this puzzle is to find a way to arrange 8 queens on a chess
    board such that no two queens can run into each other in any one move.
    To accomplish this I have allowed users to put queens on the board with
    a left click, remove them with a right click, and clear the board.
    There is also a hint view which makes it easier for the user to see
    valid moves. If you need a solution for testing purposes there are a
    few here: http://en.wikipedia.org/wiki/Eight_queens_puzzle.

---
+++ Lab4/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Lab4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Hint = new System.Windows.Forms.CheckBox();
+            this.Clear = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Hint
+            // 
+            this.Hint.AutoSize = true;
+            this.Hint.Location = new System.Drawing.Point(12, 19);
+            this.Hint.Name = "Hint";
+            this.Hint.Size = new System.Drawing.Size(50, 17);
+            this.Hint.TabIndex = 0;
+            this.Hint.Text = "Hints";
+            this.Hint.UseVisualStyleBackColor = true;
+            this.Hint.CheckedChanged += new System.EventHandler(this.Hint_CheckedChanged);
+            this.Hint.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Hint_MouseClick);
+            // 
+            // Clear
+            // 
+            this.Clear.Location = new System.Drawing.Point(90, 13);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(75, 23);
+            this.Clear.TabIndex = 1;
+            this.Clear.Text = "Clear";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(171, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(166, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "You have 0 queens on the board.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(620, 621);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.Hint);
+            this.Name = "Form1";
+            this.Text = "8-Queens Puzzle - Zach Sarkis";
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox Hint;
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
